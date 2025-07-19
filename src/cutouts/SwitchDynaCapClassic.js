dd9c6aeb61e81f8f3abab31e7d98b6b628961c6f
cleverkeebs <158592908+cleverkeebs@users.noreply.github.com>
2025-07-19 00:45:38 -0400

    f

--- src/cutouts/SwitchDynaCapClassic.js
+++ src/cutouts/SwitchDynaCapClassic.js
@@ -58,7 +58,7 @@ export class SwitchDynaCapClassic extends CutoutGenerator {
             }
 
             // Apply the fillet radius (replace chamfer1u with fillet)
-            // makerjs.path.combineFillet(model.models.outline, fillet1u.toNumber());
+            makerjs.path.combineFillet(model.models.outline, fillet1u.toNumber());
         }
 
         // 2u housing
